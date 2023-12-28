97e51f548ce529ecb6e6d3826eaacd0f86bc51e2
spo0ds <asmitadhikari540@gmail.com>
2023-12-28 15:20:44 +0545

    feat: timelock contract

---
+++ src/TimeLock.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity 0.8.23;
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract TimeLock is TimelockController {
+    constructor(
+        uint minDelay,
+        address[] memory proposers,
+        address[] memory executors,
+        address admin
+    ) TimelockController(minDelay, proposers, executors, admin) {}
+}
